8a5dca308cf999c9f10ecaeda0b9f6d53721f113
pjang77 <136946117+pjang77@users.noreply.github.com>
2023-09-12 13:12:14 -0400

    updated index.js file with all logic

---
+++ index.js
@@ -0,0 +1,62 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+async function getUserInput() {
+  const userInput = await inquirer.prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Please enter up to three characters:",
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Please enter text color (keyword or hex number):",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Please choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Please enter shape color (keyword or hex number):",
+    },
+  ]);
+  return userInput;
+}
+
+function generateSVG(text, textColor, shape, shapeColor) {
+  return `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="300" height="200" fill="${shapeColor}" />
+        <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${text}</text>
+      </svg>
+    `;
+}
+
+async function createLogo(userInput) {
+  const svgContent = generateSVG(
+    userInput.text,
+    userInput.textColor,
+    userInput.shape,
+    userInput.shapeColor
+  );
+
+  // Write the SVG content to a file named "logo.svg"
+  fs.writeFileSync("logo.svg", svgContent);
+
+  console.log("Generated logo in SVG");
+}
+
+(async () => {
+  try {
+    const userInput = await getUserInput();
+    await createLogo(userInput);
+  } catch (error) {
+    console.error("Error", error);
+  }
+})();
